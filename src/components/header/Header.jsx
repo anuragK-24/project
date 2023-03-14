c9f268ee86a36d1b370bd87e2e50be0368939c03
hfr58gb <24sept.anurag@gmail.com>
2023-03-14 13:19:28 +0530

    v1.1

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./header.css"
+export default function Header() {
+  return (
+    <div>
+<div className="nav-wrapper">
+    <div className="box-1">
+        <a href=""><img src="" alt="" /></a>
+    </div>
+    <div className="box-2">
+        <center>
+            <a href="#">Reputation</a>
+            <a href="#">New users</a>
+            <a href="#">Voters</a>
+            <a href="#">Editors</a>
+            <a href="#">Moderators</a>
+
+        </center>
+    </div>
+  </div>
+    </div>
+  )
+}
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../../components/card/Card";
-import Header from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 import { Link } from "react-router-dom";
 import "./userList.css";
 
